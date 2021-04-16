62562daf0df393d7ff9518b6b6a87abc202444e0
Aaron Williams <aaronrw4@yahoo.com>
2021-04-15 20:56:19 -0700

    Updates

---
+++ precache-manifest.9c6d5f311cd425d3b5e520053275a37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0b6ab97aae7c9478fb3be3d76386ec",
+    "url": "/Car-Sales/index.html"
+  },
+  {
+    "revision": "08496788fcaddadf2325",
+    "url": "/Car-Sales/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "e0fa92c6b84f836353b1",
+    "url": "/Car-Sales/static/css/main.c0d74d11.chunk.css"
+  },
+  {
+    "revision": "08496788fcaddadf2325",
+    "url": "/Car-Sales/static/js/2.2171e69a.chunk.js"
+  },
+  {
+    "revision": "e0fa92c6b84f836353b1",
+    "url": "/Car-Sales/static/js/main.4a035af0.chunk.js"
+  },
+  {
+    "revision": "1f9dcd49ef87d4733a65",
+    "url": "/Car-Sales/static/js/runtime~main.94ca7d13.js"
+  }
+]);
